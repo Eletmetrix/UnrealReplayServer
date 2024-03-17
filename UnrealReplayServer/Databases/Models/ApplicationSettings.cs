e4c362e53c2cfdd66f5a67c28eb8946ce1f46a02
Eletmetrix <erdemerdemmc@gmail.com>
2024-03-17 18:23:18 +0300

    added application settings but not used yet

---
+++ UnrealReplayServer/Databases/Models/ApplicationSettings.cs
@@ -0,0 +1,29 @@
+/*
+The MIT License (MIT)
+Copyright (c) 2021 Henning Thoele
+*/
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnrealReplayServer.Databases.Models
+{
+    public class ApplicationSettings
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        public string AuthorizationHeaderValue { get; set; }
+
+        public int RemainingUse { get; set; } = -1;
+
+        public int ExpirationDateAsMinute { get; set; } = 0;
+
+        public bool bUseRemainingUse { get; set; } = false;
+
+        public bool bExpirationDate { get; set; } = false;
+
+        public DateTimeOffset CreationDate { get; set; } = DateTimeOffset.UtcNow;
+    }
+}
--- UnrealReplayServer/Databases/Models/DatabaseContext.cs
+++ UnrealReplayServer/Databases/Models/DatabaseContext.cs
@@ -13,17 +13,20 @@ public class DatabaseContext : DbContext
     public DbSet<AuthorizationHeader> authorizationHeaders { get; set; }
     public DbSet<SessionFile> sessionFiles { get; set; }
     public DbSet<SessionViewer> sessionViewer { get; set; }
+    public DbSet<ApplicationSettings> applicationSettings { get; set; }
 
     public DatabaseContext(DbContextOptions<DatabaseContext> options)
         : base(options)
     {
-        sessionList = Set<Session>();
-        eventList = Set<EventEntry>();
-        authorizationHeaders = Set<AuthorizationHeader>();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Session>().HasKey(x => x.Id);
+        modelBuilder.Entity<EventEntry>().HasKey(x => x.Id);
+        modelBuilder.Entity<AuthorizationHeader>().HasKey(x => x.Id);
+        modelBuilder.Entity<ApplicationSettings>().HasKey(x => x.Id);
+
         modelBuilder.Entity<SessionFile>(x =>
         {
             x.HasKey(y => y.Id);
